569f7d75b129b3523697a565ff5a9c7d96550454
Micaela Murrie <micaelamurrie@gmail.com>
2025-07-29 14:43:57 -0300

    Prueba de jsPDF: se agregó botón para generar PDF

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,31 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import { jsPDF } from "jspdf";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const generarPDF = () => {
+    const doc = new jsPDF();
+    doc.text("Hola equipo2-tarde desde jsPDF!", 10, 10);
+    doc.save("documento.pdf");
+  };
 
   return (
     <>
-      <div>
-      </div>
+      <div></div>
       <h1>Proyecto para pruebas de concepto</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-
       </div>
 
+      <div>
+        <button onClick={generarPDF}>Generar PDF</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
