26e7ed19f0b8b25d1ba192c6c4310774fabe5aac
JorgeCarrascoF <jorgecarfacpsico@gmail.com>
2025-07-31 10:08:11 +0200

    formularios de login y registro

--- frontend/src/routes/Login.jsx
+++ frontend/src/routes/Login.jsx
@@ -1,9 +1,11 @@
+import LoginForm from "../components/LoginForm";
 import NavButton from "../components/NavButton";
 
 const Login = () => {
   return (
     <>
       <h1>Login</h1>
+      <LoginForm />
       <NavButton text="Go to Home" route="/" />
     </>
   );
--- frontend/src/routes/Register.jsx
+++ frontend/src/routes/Register.jsx
@@ -1,9 +1,11 @@
 import NavButton from "../components/NavButton";
+import RegisterForm from "../components/RegisterForm";
 
 const Register = () => {
   return (
     <>
       <h1>Register</h1>
+      <RegisterForm />
       <NavButton text="Go to Home" route="/" />
     </>
   );
