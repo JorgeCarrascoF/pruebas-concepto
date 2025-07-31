7ba0ca66263fe452bcc9049e23935dc29c799623
nicolas acuña <nicolassimonacuna@gmail.com>
2025-07-31 15:38:57 -0400

    rutas login y register

--- app.js
+++
@@ -1,9 +0,0 @@
-var msg = 'Hola Mundo!';
-console.log(msg);
-
-const express = require('express');
-const app = express();
-const auth = require('./routes/auth');
-
-app.use(express.json());
-app.use('/api', auth);
